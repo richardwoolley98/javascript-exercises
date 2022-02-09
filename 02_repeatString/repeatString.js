995af31448ae5fbcc5c603f97286d9fdd30b26de
Richard Woolley <richardwoolley98@gmail.com>
2022-02-09 22:22:46 +0000

    Exercise 2 Completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        var emptyString = "";
+        for (let i = 0; i < num; i++) {
+            emptyString = emptyString.concat(string);
+        }
+        return emptyString;
+    }
+    else
+        return "ERROR"
 
 };
-
 // Do not edit below this line
 module.exports = repeatString;
+
+

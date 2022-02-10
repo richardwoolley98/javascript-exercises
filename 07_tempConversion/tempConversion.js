99af48a4662c39153ccf6a915045c3d56945348a
Richard Woolley <richardwoolley98@gmail.com>
2022-02-10 15:41:28 +0000

    Exercise 7 Completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  const c = (temp - 32) * (5/9);
+  return parseFloat(c.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+ const f = ((9/5) * temp) + 32;
+ return parseFloat(f.toFixed(1))
 };
 
 // Do not edit below this line

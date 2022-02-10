cc2d7a877900e22e5997de7d75e87a9f89986d2f
Richard Woolley <richardwoolley98@gmail.com>
2022-02-10 14:14:22 +0000

    Exercise 3 Completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const splitString = string.split("");
+    const reverseString = splitString.reverse();
+    const finalString = reverseString.join("");
+    return finalString;
 };
 
 // Do not edit below this line

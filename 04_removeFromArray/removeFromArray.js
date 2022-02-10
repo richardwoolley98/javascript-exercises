71bc1bdd3b6ea552dd09f26a4fde1266dd04b49d
Richard Woolley <richardwoolley98@gmail.com>
2022-02-10 14:58:31 +0000

    Exercise 4 Completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(...args) {
+    const mainArray = args[0];
+    var newArray = [];
+    mainArray.forEach(element => {
+        if (! args.includes(element)) {
+            newArray.push(element);
+        }
+    });
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+

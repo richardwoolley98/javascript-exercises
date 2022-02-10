36af30c16c0e01d85e1f037d2d32e73573f6e209
Richard Woolley <richardwoolley98@gmail.com>
2022-02-10 15:13:51 +0000

    Exercise 5 Completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(first, last) {
+    if (first < 0 || last < 0) {
+        return "ERROR";
+    }
+    else if (typeof first != "number" || typeof last != "number") {
+        return "ERROR";
+    }
+    else {
+    var finalSum = 0;
+    if (last > first) {
+        for (i = first; i <= last; i++) {
+            finalSum += i;
+        }
+    }
+    else {
+        for (i = last; i <= first; i++) {
+            finalSum += i;
+        }
+    }
+    return finalSum;
+    }
 };
 
 // Do not edit below this line
